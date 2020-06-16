bc6dfc243d377f2093f2b7fec71a45323dabd13a
David Tolnay <dtolnay@gmail.com>
2020-06-16 11:51:57 -0700

    Release 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_path_to_error/0.1.2")]
+#![doc(html_root_url = "https://docs.rs/serde_path_to_error/0.1.3")]
 
 use serde::de::{self, Deserialize, DeserializeSeed, Visitor};
 use std::error::Error as StdError;
