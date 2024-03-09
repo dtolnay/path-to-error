e58a63177fd7b5683100575e48e008a4e2dbb11b
David Tolnay <dtolnay@gmail.com>
2024-03-09 10:34:06 -0800

    Release 0.1.16

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_path_to_error/0.1.15")]
+#![doc(html_root_url = "https://docs.rs/serde_path_to_error/0.1.16")]
 #![allow(
     clippy::doc_link_with_quotes, // https://github.com/rust-lang/rust-clippy/issues/8961
     clippy::iter_not_returning_iterator, // https://github.com/rust-lang/rust-clippy/issues/8285
